0ec81735f74470e1041e9845dc983d2417484c60
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2023-02-11 16:06:04 +0100

    a

--- main.cpp
+++ main.cpp
@@ -2,22 +2,20 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  // !Skapar windows skärmen
+  sf::RenderWindow window(sf::VideoMode(1280, 720), "Game");
 
+  // !Game loop
   while (window.isOpen())
   {
     sf::Event event;
+
     while (window.pollEvent(event))
     {
       if (event.type == sf::Event::Closed)
-        window.close();
+      {
+      }
     }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
   }
 
   return 0;
